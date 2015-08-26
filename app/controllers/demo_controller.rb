8dba6afb27e61bd8785dc470bc9f0258a5601670
Ross Litzenberger <codingclasses.049@gmail.com>
2015-08-26 09:55:22 -0500

    messing with outputs

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,6 +1,17 @@
 class DemoController < ApplicationController
   layout false
-  
+
   def index
+    render(:template => 'demo/hello')
+  end
+
+  def hello
+    # render('index')
+  end
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+  def lynda
+    redirect_to("http://lynda.com")
   end
 end
